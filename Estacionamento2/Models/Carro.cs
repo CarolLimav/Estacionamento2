e26fd6eaf3d7e300ad85cef72b8186124f031968
aguia <aguia@AGUIARLS>
2022-12-12 17:23:59 -0300

    Adicionar arquivos de projeto.

---
+++ Estacionamento2/Models/Carro.cs
@@ -0,0 +1,20 @@
+﻿namespace Estacionamento2.Models
+{
+    public class Carro
+    {
+        public string Cor { get; set; }
+        public string Placa { get; set; }
+
+        public string Modelo {get;set; }
+
+        public Carro(string Cor, string Placa, string Modelo) 
+        { 
+            this.Cor = Cor;
+            this.Placa = Placa;
+            this.Modelo = Modelo;
+        
+        }
+
+        public Carro() { }
+    }
+}
---
+++ Estacionamento2/Models/Garagem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento2.Models
+{
+    internal class Garagem
+    {
+        private decimal PrecoFixo { get; } = 9;
+        private decimal PrecoPorHora { get; } = 12;
+        private List<Carro> Carros  = new List<Carro>();
+
+        public Garagem()
+        {
+            
+        }
+
+        public void AdicionarVeiculo()
+        {
+            //armazenando a placa
+            Console.WriteLine("Digite a placa do carro:");
+           string  PlacaCarro = Console.ReadLine();
+            Console.WriteLine("qual o modelo do carro?");
+            string ModeloCarro= Console.ReadLine();
+            Console.WriteLine("Qual a cor do carro:");
+            string CorCarro = Console.ReadLine();
+
+
+            Carro novoCarro = new Carro(CorCarro,PlacaCarro,ModeloCarro);
+            Carros.Add(novoCarro);
+            Console.WriteLine("o carro sera adicionado!");
+
+        }
+        
+        public void RemoverVeiculo()
+        {
+        //pedindo a placa p/remover
+            Console.WriteLine("qual a placa do carro que deseja remover?");
+            string PlacaCarro= Console.ReadLine();  
+          //procurar o carro com a placa digitada
+          foreach(var carro in Carros)
+            {
+                if(carro.Placa== PlacaCarro)
+                {
+                    Carros.Remove(carro);
+                    break;
+               
+                }
+            }
+            Console.WriteLine("O carro foi removido com sucesso!");
+        }
+        // listagem de veiculos 
+        
+        public void ListarVeiculo()
+        {
+            foreach(var carro in Carros)
+            {
+                Console.WriteLine("Placa:"+carro.Placa);
+                Console.WriteLine("Modelo:"+carro.Modelo);
+                Console.WriteLine("Cor:"+carro.Cor);
+            }
+        }
+    }
+}
